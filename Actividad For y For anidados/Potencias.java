10e8fb31abbe5abe840b919097910861a03c64ea
Wendoline-Aguilar <arw54110@gmail.com>
2025-11-10 16:11:44 -0600

    Add files via upload

---
+++ Actividad For y For anidados/Potencias.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Potencias{
+	public static void main(String args[]){
+	Scanner sc=new Scanner (System.in);
+	int b, p, i;
+	float re=1;
+	System.out.println("Escriba el número base: ");
+	b=sc.nextInt();
+	System.out.println("Escriba la potencia: ");
+	p=sc.nextInt();
+	for (i=1; i<=p; i++){
+		 	re=(re*b);
+	}
+	System.out.println("El resultado es: " + re);
+	sc.close();
+	}
+}
